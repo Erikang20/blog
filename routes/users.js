efaffe610fa91a9c2b46e9553f867d0f481d5f77
Erikang20 <erikang20@gmail.com>
2016-06-16 16:45:49 -0500

    add more users

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,32 @@ router.get( '/', function( req, res ) {
 	} );
 } );
 
+
+//new users
+router.get( '/new', function( req, res ) {
+	res.render( 'new' );
+} );
+
+
+///post to users
+router.post( '/', function( req, res ) {
+	var user = req.body;
+	// eval( locus )
+	knex( 'users' ).insert( {
+		name: user.name,
+		userName: user.nickname
+	} ).then( function( result, err ) {
+		res.redirect( '/users' );
+	} );
+} );
+
+router.get( '/:id', function( req, res ) {
+	var userId = req.params.id;
+
+	knex( 'users' ).where( 'id', userId ).first().then( function( result, err ) {
+		var user = result;
+		res.render( 'users/index' );
+	} );
+} );
+
 module.exports = router;
