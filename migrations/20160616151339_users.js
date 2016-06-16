9cf56034e29236fe4e46507822d7927ddf4e3e3b
Erikang20 <erikang20@gmail.com>
2016-06-16 15:20:15 -0500

    first stuff

---
+++ migrations/20160616151339_users.js
@@ -0,0 +1,17 @@
+exports.up = function( knex, Promise ) {
+
+	return knex.schema.createTable( 'users', function( table ) {
+		table.increments();
+		table.string( 'name' );
+		table.string( 'userName' );
+		table.timestamp( 'created_at' ).defaultTo( knex.fn.now() );
+
+	} );
+
+};
+
+exports.down = function( knex, Promise ) {
+	return knex.schema.dropTable( 'users' );
+
+
+};
