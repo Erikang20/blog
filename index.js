c8902763a154f117b6a7c932115cd5fcffe5e73f
Erikang20 <erikang20@gmail.com>
2016-06-17 14:16:44 -0500

    add stuff

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require( 'dotenv' ).config();
 app.set( 'view engine', 'ejs' );
 app.set( 'views', ( __dirname + '/views' ) );
 app.use( express.static( __dirname + '/public' ) );
-app.use( morgan( 'dev' ) );
+// app.use( morgan( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( {
 	extended: false
