59f76b54fbb93579d3fe82c79f277d848df6c380
Erikang20 <erikang20@gmail.com>
2016-06-20 11:05:55 -0500

    fixed edit path

--- routes/users.js
+++ routes/users.js
@@ -15,12 +15,6 @@ router.get( '/', function( req, res ) {
 } );
 
 
-//add new users
-router.get( '/new', function( req, res ) {
-	res.render( 'new' );
-} );
-
-
 ///post to users table
 router.post( '/', function( req, res ) {
 	var user = req.body;
@@ -34,10 +28,16 @@ router.post( '/', function( req, res ) {
 } );
 
 
+//add new users
+router.get( '/new', function( req, res ) {
+	res.render( 'new' );
+} );
+
+
+
 ///get user id
 router.get( '/:id', function( req, res ) {
 	var userId = req.params.id;
-
 	knex( 'users' ).where( 'id', userId ).first().then( function( result, err ) {
 		var user = result;
 		// eval( locus )
@@ -49,20 +49,19 @@ router.get( '/:id', function( req, res ) {
 
 
 ///update users///
-router.post( '/:id', function( req, res ) {
-	var userId = req.params.id;
-	var user = req.body;
-	knex( 'users' ).where( 'id', userId ).first().update( {
-
-		name: user.name,
-		userName: user.userName
-	}, 'id' ).then( function( result, err ) {
-		res.redirect( 'users' );
-		// eval( locus );
-
-	} );
-} );
-
+// router.post( '/:id', function( req, res ) {
+// 	var userId = req.params.id;
+// 	var user = req.body;
+// 	knex( 'users' ).where( 'id', userId ).first().update( {
+//
+// 		name: user.name,
+// 		userName: user.userName
+// 	}, 'id' ).then( function( result, err ) {
+// 		res.redirect( 'users' );
+// 		// eval( locus );
+//
+// 	} );
+// } );
 
 
 //edit
@@ -76,14 +75,14 @@ router.get( '/:id/edit', function( req, res ) {
 	} );
 } );
 
-router.post( '/:id/edit', function( req, res ) {
+router.post( '/:id', function( req, res ) {
 	var userId = req.params.id;
 	var user = req.body;
 	knex( 'users' ).where( 'id', userId ).first().update( {
 		name: user.name,
-		userName: user.nickname
+		userName: user.userName
 	} ).then( function( result ) {
-		res.redirect( '/edit' );
+		res.redirect( 'edit' );
 	} );
 } );
 
